ed25a4dac17b9b87910745fe613b48b291a9493b
Lilly Oh <ohlilly@gmail.com>
2020-04-15 22:06:26 -0400

    added query selectors for hiding and showing the relevant divs

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const { hash } = window.location;
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+  document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
 
   const input = document.querySelector('#message-input');
   const encoded = btoa(input.value);
