21916c18f641460c0f1d77ea849f753f7a9f0f7c
Lilly Oh <ohlilly@gmail.com>
2020-04-15 21:21:00 -0400

    added dom selectors and encoding

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
 
-  const input = document.querySelector('input');
+  const input = document.querySelector('#message-input');
+  const encoded = btoa(input.value);
+
+  const link = document.querySelector('#link-input');
+  link.value = `${window.location}#${encoded}`
+  link.select();
+  document.execCommand('copy');
 
 });
